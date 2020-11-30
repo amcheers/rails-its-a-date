c03966d871b51d582dd9014aee23cb5266e3e575
Ahmad Cheers <acheers1@gmail.com>
2020-11-30 21:02:29 +0100

    created category model, migrated db.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_195626) do
+ActiveRecord::Schema.define(version: 2020_11_30_200122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "date_activities", force: :cascade do |t|
     t.string "title"
