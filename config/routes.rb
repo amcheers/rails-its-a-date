c884b5e90f42278c699843dc5e0737df5c7e9bd8
Ahmad Cheers <acheers1@gmail.com>
2020-11-30 23:17:45 +0100

    Constructed all non polymorphic routes from user stories.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :dashboard, only: [:index] do
+    resources :dates, only: [:new, :create, :edit, :update, :destroy]
+  end
+  resources :packages, only: [:index, :show, :edit, :update]
+  get '/dates', to: 'dates#index', as: 'dates_index'
+  get '/dates/:id', to: 'dates#show', as: 'dates_show'
 end
