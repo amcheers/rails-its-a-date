5821466009b02924b14a1509305e43f99640d153
PauliPa42 <70962270+PauliPa42@users.noreply.github.com>
2020-12-01 10:50:34 +0100

    Update 20201130203410_create_reviews.rb

--- db/migrate/20201130203410_create_reviews.rb
+++ db/migrate/20201130203410_create_reviews.rb
@@ -3,6 +3,7 @@ class CreateReviews < ActiveRecord::Migration[6.0]
     create_table :reviews do |t|
       t.text :content
       t.integer :rating
+      t.references :activity, polymorphic: true, index: true
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
