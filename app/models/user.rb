a3ec9701782be030f7f4241a5cefcdd87712ff5e
Paulina Paetzold <paulinapaetzold@gmx.de>
2020-12-01 16:11:15 +0100

    added photo to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :reviews
   has_many :wishlists
+  has_one_attached :photo
 
   # for all users
   validates :username, presence: true, uniqueness: true
