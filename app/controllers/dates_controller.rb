448cb02531093581df91c3d52fe3aa13adcb5664
jonasmuecke <jonas@muecke.com>
2020-12-01 14:09:59 +0100

    added new, create, edit, update action for dates controller and their views

--- app/controllers/dates_controller.rb
+++ app/controllers/dates_controller.rb
@@ -1,2 +1,33 @@
 class DatesController < ApplicationController
+  def new
+    @date = Dateactivity.new
+  end
+
+  def create
+    @date = Dateactivity.new(date_params)
+    @date.user = current_user
+    if @date.save
+      redirect_to date_path(@date)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @date.update(date_params)
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def set_date
+    @date = Dateactivity.find(params[:id])
+  end
+
+  def date_params
+    params.require(:date_activitie).permit(:title, :description, :price, :reservation_url, :contact_email, :availability, :location, :confirmed, :reservation)
+  end
 end
