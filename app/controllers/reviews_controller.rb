31ee1bb8113cc44507c9c40242191086750e6115
Paulina Paetzold <paulinapaetzold@gmx.de>
2020-12-02 15:57:41 +0100

    addes search bar for home page, added filter option, added reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,33 @@
 class ReviewsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
+  before_action :set_activity
 
   def index
+    @reviews = @activity.reviews
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.activity = @activity
+    if @review.save
+      redirect_to reviews_path(@activity)
+    else
+      :new
+    end
+  end
+
+  private
+
+  def set_activity
     activity_class = params[:activity_type].camelcase.constantize
     @activity = activity_class.find(params[:activity_id])
-    @reviews = Review.all
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
   end
 end
