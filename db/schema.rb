58da34f0c587b7ad2e66ec2a057a6f9a128c08d5
Ahmad Cheers <acheers1@gmail.com>
2020-12-02 14:54:41 +0100

    Created and ran migration to change name column in packages to title.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_134526) do
+ActiveRecord::Schema.define(version: 2020_12_02_135109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,7 +73,7 @@ ActiveRecord::Schema.define(version: 2020_12_01_134526) do
   end
 
   create_table "packages", force: :cascade do |t|
-    t.string "name"
+    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
