bba0e80c162138af7023dcb457b4b874448a2d17
Paulina Paetzold <paulinapaetzold@gmx.de>
2020-12-10 16:23:35 +0100

    fixed dash

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   def show
     @user = current_user
+    @my_packages = Package.where(user_id: current_user.id)
     @all_owned = current_user.date_activities
     @liked_dates = current_user.favorite_activities.where(activity_type: "DateActivity")
     @liked_packages = current_user.favorite_activities.where(activity_type: "Package")
